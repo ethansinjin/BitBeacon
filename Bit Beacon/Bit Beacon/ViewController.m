436c8b96a1823bdb37c2f0e7d69e9b66599527f1
JonahStarling <starling.jonah@gmail.com>
2014-09-20 16:56:47 -0400

    Version 0.25
    Fixed UI and added changes when the button is pressed.

--- Bit Beacon/Bit Beacon/ViewController.m
+++ Bit Beacon/Bit Beacon/ViewController.m
@@ -10,12 +10,15 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *statusIcon;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +26,18 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void) updateStatus{
+    
+        [_chargeButton setTitle:@"Charging..." forState:UIControlStateDisabled];
+        [_chargeButton setEnabled:NO];
+        [_statusIcon setImage:[UIImage imageNamed:@"signal"]];
+
+}
+
+- (IBAction)buttonTapped:(UIButton *)sender {
+    [self updateStatus];
+}
+
 
 @end
