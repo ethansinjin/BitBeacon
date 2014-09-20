1146527036f6eef39c33aa9ed0321b7dfd832b59
Ethan Gill <ethan.gill@me.com>
2014-09-19 23:26:15 -0400

    even other commit

--- BitBoat/BitBoat/ViewController.m
+++ BitBoat/BitBoat/ViewController.m
@@ -7,16 +7,46 @@
 //
 
 #import "ViewController.h"
+#import "FBShimmering/FBShimmeringView.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+    FBShimmeringView *_shimmeringView;
+    UILabel *_logoLabel;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _shimmeringView = [[FBShimmeringView alloc] init];
+    _shimmeringView.shimmering = YES;
+    _shimmeringView.shimmeringBeginFadeDuration = 0.3;
+    _shimmeringView.shimmeringOpacity = 0.3;
+    [self.view addSubview:_shimmeringView];
+    
+    _logoLabel = [[UILabel alloc] initWithFrame:_shimmeringView.bounds];
+    _logoLabel.text = @"Pay with BitPay";
+    _logoLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:45.0];
+    _logoLabel.textColor = [UIColor whiteColor];
+    _logoLabel.textAlignment = NSTextAlignmentCenter;
+    _logoLabel.backgroundColor = [UIColor clearColor];
+    _shimmeringView.contentView = _logoLabel;
+    
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    CGRect shimmeringFrame = self.view.bounds;
+    shimmeringFrame.origin.y = shimmeringFrame.size.height * 0.68;
+    shimmeringFrame.size.height = shimmeringFrame.size.height * 0.32;
+    _shimmeringView.frame = shimmeringFrame;
 }
 
 - (void)didReceiveMemoryWarning {
